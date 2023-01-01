f000a7ea485f82c4ec1c3d81c1816484440ad6c7
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-02 00:13:06 +0300

    fix : rename

--- design_patterns/behavioral/observer/lib/core/subscriber/default_stock_subs.dart
+++ design_patterns/behavioral/observer/lib/core/subscriber/default_stock_subs.dart
@@ -2,7 +2,7 @@ import 'package:observer/core/stock/stock.dart';
 import 'package:observer/core/subscriber/stock_subscriber.dart';
 
 class DefaultStockSubs extends StockSubs {
-  DefaultStockTicker() {
+  DefaultStockSubs() {
     stockTitle = 'All Stocks';
   }
 
