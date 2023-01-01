66e3c03ec6ee7f2a8c49cee68904a895b47bd388
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-01 23:36:22 +0300

    add: setStock is defined in the StockTicker class.

--- design_patterns/behavioral/observer/lib/core/tickers/stock_ticker.dart
+++ design_patterns/behavioral/observer/lib/core/tickers/stock_ticker.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:observer/core/enums/stock_ticker_acronyms.dart';
 import 'package:observer/core/stock/stock.dart';
 import 'package:observer/core/subscriber/stock_subscriber.dart';
 
@@ -21,5 +23,11 @@ abstract class StockTicker {
     for (final subs in _subs) {
       subs.update(stock!);
     }
+  }
+
+  void setStock(StockTickerAcronyms stockTickerAcronyms, int min, int max) {
+    final lastStock = stock;
+    final fakePrice = faker.randomGenerator.integer(max, min: min) / 200;
+    final changeAmount = lastStock != null ? fakePrice - lastStock.price : 0.0;
   }
 }
