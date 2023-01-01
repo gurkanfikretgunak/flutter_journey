cc219b811700c9087061f45e8fbdd8584e97f7a3
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-01 23:30:24 +0300

    add:StockSubs class impl. and some functions

--- design_patterns/behavioral/observer/lib/core/tickers/stock_ticker.dart
+++ design_patterns/behavioral/observer/lib/core/tickers/stock_ticker.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:observer/core/stock/stock.dart';
+import 'package:observer/core/subscriber/stock_subscriber.dart';
 
 abstract class StockTicker {
   late final String stockTitle;
@@ -9,4 +10,16 @@ abstract class StockTicker {
 
   @protected
   Stock? stock;
+
+  final _subs = <StockSubs>[];
+
+  void sub(StockSubs subscriber) => _subs.add(subscriber);
+
+  void unsub(StockSubs subscriber) =>
+      _subs.removeWhere((s) => s.id == subscriber.id);
+  void notifySubs() {
+    for (final subs in _subs) {
+      subs.update(stock!);
+    }
+  }
 }
