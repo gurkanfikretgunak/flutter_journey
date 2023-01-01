21b928da19899b0ed899123f857116e9c65d8647
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-02 01:41:46 +0300

    add: Contact Model

---
+++ design_patterns/structural/adapter/lib/core/model/contact_model.dart
@@ -0,0 +1,15 @@
+class Contact {
+  final String name;
+  final String surname;
+  final String fullName;
+  final String email;
+  final bool favourite;
+
+  const Contact({
+    required this.name,
+    required this.surname,
+    required this.fullName,
+    required this.email,
+    required this.favourite,
+  });
+}
