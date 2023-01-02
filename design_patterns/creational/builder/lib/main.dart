ed4c814b4fb10f6924c42881cd01e4ba437d1c1b
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-02 13:09:01 +0300

    add: MaterialApp

--- design_patterns/creational/builder/lib/main.dart
+++ design_patterns/creational/builder/lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        //  home: FoodView(),
+        );
+  }
+}
