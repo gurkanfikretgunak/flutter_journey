c1222e887e0c1829e031298792f79228454037e1
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-05 02:06:42 +0300

    add: Runner and example func.

--- algorithms /complexity.dart
+++ algorithms /complexity.dart
@@ -1,1 +1,9 @@
-// Complexity 
+// Complexity
+
+getFirst(List<String> anyStringValues) {
+  if (anyStringValues.isNotEmpty) {
+    print(anyStringValues.first);
+  } else {
+    print('Dont have any String Value 🤷‍♂️ ');
+  }
+}
---
+++ algorithms /runner/bin/runner.dart
@@ -0,0 +1,5 @@
+import '../../complexity.dart';
+
+void main(List<String> arguments) {
+  getFirst([]);
+}
