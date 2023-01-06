f016823655bb0596370d74a45dfe1bf4288c6832
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-06 23:48:13 +0300

    upgrade: main.dart

--- design_patterns/creational/prototype/lib/main.dart
+++ design_patterns/creational/prototype/lib/main.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:prototype/view/prototype.view.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PrototypeView(),
+    );
+  }
+}
