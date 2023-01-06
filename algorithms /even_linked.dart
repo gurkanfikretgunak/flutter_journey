fca8b6a638b23b86694e825cb9b7c70d106fb772
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2023-01-06 15:04:24 +0300

    add: Even linked append,push,pop and removeLastOne

--- algorithms /even_linked.dart
+++ algorithms /even_linked.dart
@@ -21,3 +21,100 @@ abstract class Linkeds<G> {
   G? pop();
   G? removeLastOne();
 }
+
+class EvenLinkeds<G> extends Iterable<G> implements Linkeds<G> {
+  @override
+  Node<G>? start;
+
+  @override
+  Node<G>? chain;
+
+  @override
+  bool get isEmpty => start == null;
+
+  @override
+  void append(G value) {
+    final newNode = Node(value: value, following: chain);
+
+    if (isEmpty) {
+      start = newNode;
+    } else {
+      start!.following = newNode;
+
+      start?.following ??= newNode;
+    }
+    start = newNode;
+  }
+
+  @override
+  void push(G value) {
+    final newNode = Node(value: value, following: start);
+
+    if (isEmpty) {
+      start = newNode;
+    } else {
+      start!.following = newNode;
+
+      start?.following ??= newNode;
+    }
+    start = newNode;
+  }
+
+  @override
+  G? pop() {
+    if (isEmpty) return null;
+
+    final value = start?.value;
+
+    if (start?.following == null) {
+      start = null;
+      chain = null;
+      return value;
+    }
+
+    start = start?.following;
+    start?.following = null;
+
+    return value;
+  }
+
+  @override
+  G? removeLastOne() {
+    if (chain?.following == null) return pop();
+
+    final value = start?.value;
+
+    start = chain?.following;
+    start?.following = null;
+
+    return value;
+  }
+
+  @override
+  Iterator<G> get iterator => _LinkedsIterator(this);
+
+  @override
+  String toString() => '[${join(', ')}]';
+}
+
+class _LinkedsIterator<G> implements Iterator<G> {
+  _LinkedsIterator(EvenLinkeds<G> list) : _list = list;
+  final EvenLinkeds<G> _list;
+  Node<G>? _currentNode;
+  bool _firstPass = true;
+
+  @override
+  G get current => _currentNode!.value;
+
+  @override
+  bool moveNext() {
+    if (_list.isEmpty) return false;
+    if (_firstPass) {
+      _currentNode = _list.start;
+      _firstPass = false;
+    } else {
+      _currentNode = _currentNode?.following;
+    }
+    return _currentNode != null;
+  }
+}
